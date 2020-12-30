930d9a44a9867515fac9e2d7908a879a15ab0183
Sean Hong(홍성민) <seanhong2000@gmail.com>
2020-12-30 12:53:49 +0900

    naver maps api _ test

--- query_naver.py
+++ query_naver.py
@@ -3,20 +3,24 @@ import pprint
 import geohash2
 from urllib.parse import urljoin
 import json
-from options import naver_url as nv
+from options import NAVER_URL as nv
 
 class NaverAPI:
     def __init__(self, query: str):
         self.url = urljoin(nv.NAVER_BASE_URL, nv.NAVER_SEARCH_URL.format(query))
-        self._json = requests.get(url).content.decode('utf-8') # raw-byte 를 utf-8로 디코딩
+        self._json = requests.get(self.url).content.decode('utf-8') # raw-byte 를 utf-8로 디코딩
 
-    def show_json(self):
-        print(self._json)
-        return self._json
+    def get_json(self):
+        # print(self._json)
+        _js = json.loads(self._json)
+        _json_list = _js['result']['site']['list']
+        
+        return _json_list
 
     def save_as_file(self):
         with open('content_json.json','w+') as file:
-            file.write(_json)
+            file.write(self.get_json())
             file.close()
 
-# print(parsed)
+nv = NaverAPI("노은역gs25")
+nv.get_json()
